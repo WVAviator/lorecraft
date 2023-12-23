caa7a6f931e0a0a377fd33e5c45195d83f7587be
Wvaviator <wvaviator@gmail.com>
2023-12-23 16:33:48 -0600

    add loading spinner to splash screen

--- src/screens/SplashLoadingScreen.tsx
+++ src/screens/SplashLoadingScreen.tsx
@@ -1,5 +1,7 @@
 import BackgroundDiv from '../components/BackgroundDiv/BackgroundDiv';
 import background from '../assets/images/splash/background.png';
+import FlexContainer from '../components/FlexContainer/FlexContainer';
+import LoadingSpinner from '../components/LoadingSpinner/LoadingSpinner';
 
 const BG_ALT_DESC =
   'A mystical leatherbound book embedded with a glowing blue gem surrounded by intricate patterns rests partially buried in sand in a desert valley surrounding by sharp mountain peaks with a glowing blue and pink aurora in the night sky';
@@ -8,7 +10,14 @@ const SplashLoadingScreen = () => {
   console.log('img: ' + background);
   return (
     <BackgroundDiv image={background} alt={BG_ALT_DESC}>
-      Test
+      <FlexContainer
+        alignItems="flex-end"
+        width="100%"
+        height="100%"
+        padding="0.5rem"
+      >
+        <LoadingSpinner />
+      </FlexContainer>
     </BackgroundDiv>
   );
 };
