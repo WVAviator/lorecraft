ba174660e55391ff1b9c004882dfcae0a4ea664b
Wvaviator <wvaviator@gmail.com>
2024-01-11 13:55:39 -0600

    add tests for tool call

--- openai_lib/src/common/tool_call.rs
+++ openai_lib/src/common/tool_call.rs
@@ -1,5 +1,7 @@
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
 
+use super::error::Error;
+
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct ToolCall {
     pub id: String,
@@ -18,7 +20,46 @@ impl ToolCall {
     pub fn get_name(&self) -> String {
         self.function.name.clone()
     }
-    pub fn extract_arguments<T: DeserializeOwned>(&self) -> Result<T, serde_json::Error> {
+    pub fn extract_arguments<T: DeserializeOwned>(&self) -> Result<T, Error> {
         serde_json::from_str::<T>(&self.function.arguments)
+            .map_err(|e| Error::DeserializationFailure(e))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn extracts_arguments_to_value() {
+        let tool_call = ToolCall {
+            id: String::from("test"),
+            type_: String::from("test"),
+            function: FunctionOutput {
+                name: String::from("test"),
+                arguments: String::from(r#"{"test": "test"}"#),
+            },
+        };
+        let arguments = tool_call.extract_arguments::<serde_json::Value>().unwrap();
+        assert_eq!(arguments["test"], "test");
+    }
+
+    #[test]
+    fn extracts_arguments_to_struct() {
+        #[derive(Serialize, Deserialize, Debug, Clone)]
+        struct Test {
+            test: String,
+        }
+
+        let tool_call = ToolCall {
+            id: String::from("test"),
+            type_: String::from("test"),
+            function: FunctionOutput {
+                name: String::from("test"),
+                arguments: String::from(r#"{"test": "test"}"#),
+            },
+        };
+        let arguments = tool_call.extract_arguments::<Test>().unwrap();
+        assert_eq!(arguments.test, "test");
     }
 }
