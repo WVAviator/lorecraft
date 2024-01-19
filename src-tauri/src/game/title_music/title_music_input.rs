08370e4280453f5398cd0955d0ebfca97d36e781
Wvaviator <wvaviator@gmail.com>
2024-01-18 19:01:55 -0600

    create title music input and main prompt

---
+++ src-tauri/src/game/title_music/title_music_input.rs
@@ -0,0 +1,29 @@
+use serde::{Deserialize, Serialize};
+
+use crate::game::{summary::Summary, Image};
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct TitleMusicInput {
+    game_summary: String,
+    game_art_description: String,
+    music_themes: Vec<String>,
+}
+
+impl TitleMusicInput {
+    pub fn new(summary: &Summary, cover_art: &Image) -> Result<Self, anyhow::Error> {
+        let game_summary = summary.summary.clone();
+        let game_art_description = match cover_art {
+            Prompt(prompt) => prompt.clone(),
+            Created { alt, .. } => alt.clone(),
+        };
+
+        // TODO: Get music themes from file
+        let music_themes = vec![];
+
+        Ok(TitleMusicInput {
+            game_summary,
+            game_art_description,
+            music_themes,
+        })
+    }
+}
